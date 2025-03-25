3a565538585e12136c13df4baeb0ed7decf84b07
Alexander Kalaj <unixdisciple.us@gmail.com>
2025-03-25 16:51:20 -0700

    Added start, stop, restore functions to example BASH script

--- minio-node.sh
+++ minio-node.sh
@@ -40,28 +40,145 @@ function start_vm {
     done
 }
 
+function stop_vm {
+    local vm_name="$1"
+    local vmx_path="${BASE_VM_DIR}/${vm_name}.vmwarevm/${vm_name}.vmx"
+
+    vmrun -T fusion stop "$vmx_path" soft
+    if [ $? -eq 0 ]; then
+        echo "Stopped $vm successfully..."
+    else
+        echo "$vm halt has failed!"
+        exit 1
+    fi
+    
+}
+
+function boot_vm {
+    local vm_name="$1"
+    local vmx_path="${BASE_VM_DIR}/${vm_name}.vmwarevm/${vm_name}.vmx"
+
+    vmrun -T fusion start "$vmx_path"
+    if [ $? -eq 0 ]; then
+        echo "Started $vm successfully..."
+    else
+        echo "$vm start has failed!"
+        exit 1
+    fi
+}
+
+function backup_vm {
+    for vm in $VIRTUAL_MACHINES_NAMES; do
+        local vmx_path="${BASE_VM_DIR}/${vm}.vmwarevm/${vm}.vmx"
+        local snapshot_name="$1"
+        local timestamp=$(date '+%Y-%m-%d')
+        local snapshot="${snapshot_name}-${timestamp}"
+
+        # Stop Machine
+        echo "Stopping virtual machine: $vm"
+        stop_vm $vm
+
+        # Snapshot Machine        
+        vmrun -T fusion snapshot "$vmx_path" $snapshot
+        if [ $? -eq 0 ]; then
+            echo "$vm snapshot '$snapshot' taken successfully..."
+        else
+            echo "$vm snapshot '$snapshot' has failed!"
+            exit 1
+        fi
+
+        # Start Machine
+        start_vm $vm
+    done   
+}
+
+function format_vm {
+    for vm in $VIRTUAL_MACHINES_NAMES; do
+        local vmx_path="${BASE_VM_DIR}/${vm}.vmwarevm/${vm}.vmx"
+        local snapshot_name="$1"
+
+        # Snapshot Machine        
+        vmrun -T fusion deleteSnapshot "$vmx_path" $snapshot_name
+        if [ $? -eq 0 ]; then
+            echo "$vm snapshot '$snapshot_name' deleted successfully..."
+        else
+            echo "$vm snapshot deletion of '$snapshot_name' has failed!"
+            exit 1
+        fi
+    done   
+}
+
+function restore_vm {
+    for vm in $VIRTUAL_MACHINES_NAMES; do
+        local vmx_path="${BASE_VM_DIR}/${vm}.vmwarevm/${vm}.vmx"
+        local snapshot_name="$1"
+
+        # Stop Machine
+        echo "Stopping virtual machine: $vm"
+        stop_vm $vm
+
+        # Snapshot Machine        
+        vmrun -T fusion revertToSnapshot "$vmx_path" $snapshot_name
+        if [ $? -eq 0 ]; then
+            echo "$vm had snapshot '$snapshot_name' restored succesfully..."
+        else
+            echo "$vm failed to restore '$snapshot_name'!"
+            exit 1
+        fi
+
+        # Start Machine
+        start_vm $vm
+    done   
+}
+
+function list_snapshots {
+    echo -e "[ Status: $(date) ]\n";
+    for vm in $VIRTUAL_MACHINES_NAMES; do
+        local vmx_path="${BASE_VM_DIR}/${vm}.vmwarevm/${vm}.vmx"
+        
+        # List VM snapshots
+        echo "Listing snapshots for: $vm"
+        vmrun -T fusion listSnapshots "$vmx_path"
+    done   
+}
+
 #################
 ### MAIN CODE ###
 #################
 
 function main {
 
-    action=$1
+    local action=$1
+    local arg=$2
 
     case $action in
       start)
         start_vm
         ;;
+      status)
+        start_vm
+        ;;
       restore)
-        restore_vm
+        restore_vm $2
+        ;;
+      stop)
+        stop_vm
+        ;;
+      format)
+        format_vm $2
+        ;;
+      list)
+        list_snapshots
         ;;
       backup)
-        backup_vm
+        backup_vm $2
         ;;
       *)
         echo '''
-        Usage: minio-node.sh [start|restore|backup]
+        Usage: minio-node.sh [start|status|restore|stop|format|list|backup]
         '''
         ;;
     esac
 }
+
+main $1 $2
